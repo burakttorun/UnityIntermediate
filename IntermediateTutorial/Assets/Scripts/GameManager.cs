40173acf9221a04947b7e2772d3934343a3aab0c
Burak Torun <burakttorun@hotmail.com>
2021-03-01 23:26:31 +0300

    An example about Ternary Operation has been added.

--- IntermediateTutorial/Assets/Scripts/GameManager.cs
+++ IntermediateTutorial/Assets/Scripts/GameManager.cs
@@ -10,11 +10,19 @@ public class GameManager : MonoBehaviour
         PlayerController myPlayer = new PlayerController();
         myPlayer.Experience = 5;
         int x = myPlayer.Experience;
+        TernaryOperator(myPlayer);
+    }
+
+     public void TernaryOperator(PlayerController myPlayer)
+    {
+        //This is an example Ternary Operation that chooses a message based on the variable "health".
+        string message = myPlayer.Health > 0 ? "Player is Alive" : myPlayer.Health == 0 ? "Player is Barely Alive" : "Player is Dead";
     }
 
     // Update is called once per frame
     void Update()
     {
+       
         
     }
 }
