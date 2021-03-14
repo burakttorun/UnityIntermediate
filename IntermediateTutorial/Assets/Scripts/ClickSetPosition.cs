1bb298f376cc6bccfff056428cc0b026214c0338
Burak Torun <burakttorun@hotmail.com>
2021-03-14 20:55:43 +0300

    An example was made where Properties and Coroutines were used together.

---
+++ IntermediateTutorial/Assets/Scripts/ClickSetPosition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickSetPosition : MonoBehaviour
+{
+    public PropertiesAndCoroutines coroutineScript;
+    void OnMouseDown()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        Physics.Raycast(ray, out hit);
+
+        if (hit.collider.gameObject == gameObject)
+        {
+            Vector3 newTarget = hit.point + new Vector3(0, 0.5f, 0);
+            coroutineScript.Target = newTarget;
+        }
+    }
+}
