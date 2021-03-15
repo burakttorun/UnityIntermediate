b950c540f1586bf2541d6483c41c0093555fa503
Burak Torun <burakttorun@hotmail.com>
2021-03-15 07:04:17 +0300

    An example was made with Quaternions.

---
+++ IntermediateTutorial/Assets/Scripts/GravityScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityScript : MonoBehaviour
+{
+    public Transform target;
+
+
+    void Update()
+    {
+        Vector3 relativePos = (target.position + new Vector3(0, 1.5f, 0)) - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
+
+        Quaternion current = transform.localRotation;
+
+        transform.localRotation = Quaternion.Slerp(current, rotation, Time.deltaTime);
+        transform.Translate(0, 0, 3 * Time.deltaTime);
+    }
+}
