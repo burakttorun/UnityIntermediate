dc75697133019f470a9f8bc9353749140cc0f91e
Burak Torun <burakttorun@hotmail.com>
2021-03-16 21:56:47 +0300

    The example with the Delegates was made.

---
+++ IntermediateTutorial/Assets/Scripts/MulticastScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MulticastScript : MonoBehaviour
+{
+    delegate void MultiDelegate();
+    MultiDelegate myMultiDelegate;
+
+
+    void Start()
+    {
+        myMultiDelegate += PowerUp;
+        myMultiDelegate += TurnRed;
+
+        if (myMultiDelegate != null)
+        {
+            myMultiDelegate();
+        }
+    }
+
+    void PowerUp()
+    {
+        print("Sphere is powering up!");
+    }
+
+    void TurnRed()
+    {
+       GetComponent<Renderer>().material.color = Color.red;
+    }
+}
