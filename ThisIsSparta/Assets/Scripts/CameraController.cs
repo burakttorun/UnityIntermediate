d7e9b8a6f9e9b4daaf0d23da5d559283e17fe1ec
Burak Torun <burakttorun@hotmail.com>
2021-03-18 18:42:37 +0300

    The preliminary scene was prepared.

---
+++ ThisIsSparta/Assets/Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    [SerializeField] private Vector3 offset = new Vector3(0, 5, -7);
+
+    private void LateUpdate()
+    {
+        //Offset the camera behind the player by adding to the player's position.
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ ThisIsSparta/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 10f;
+    private float rotationSpeed = 75f;
+    private float horizontalInput;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TurnGreen();
+        
+    }
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TurnRed();  
+        }
+    }
+
+    void TurnRed()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+    void TurnGreen()
+    {
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    void Movement()
+    {
+        //This is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        
+
+        //Move to vehicle forward.
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        //We turn the vehicle.
+        transform.Rotate(Vector3.up, Time.deltaTime * rotationSpeed * horizontalInput);
+    }
+   
+}
