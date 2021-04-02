d23547f2ab01b300a68ccd279ab90600c184286f
Burak Torun <burakttorun@hotmail.com>
2021-04-03 00:13:59 +0300

    Materials have been added.

---
+++ MatchThePlanets/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LeftButtonClicked()
+    {
+        Debug.Log("left button is clicked");
+    }
+    public void RightButtonClicked()
+    {
+        Debug.Log("right button is clicked");
+    }
+}
