848d36edd053edd2ae84011bb985c82bee966893
Burak Torun <burakttorun@hotmail.com>
2021-04-03 23:21:47 +0300

    Switching between sprites when the button is pressed is complete.

--- MatchThePlanets/Assets/Scripts/GameManager.cs
+++ MatchThePlanets/Assets/Scripts/GameManager.cs
@@ -1,9 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public Sprite[] sprites;
+
+    public Image topPlanetImage;
+    public Image bottomPlanetImage;
+    
+    private int topCount=0;
+    private int bottomCount=0;
+    private void Awake()
+    {
+       
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -16,12 +28,49 @@ public class GameManager : MonoBehaviour
 
     }
 
-    public void LeftButtonClicked()
+    public void TopLeftButtonClicked()
     {
-        Debug.Log("left button is clicked");
+        topCount--;
+
+        if (topCount <= 0)
+        {
+            topCount = sprites.Length-1;
+        }
+
+        topPlanetImage.sprite = sprites[topCount];
     }
-    public void RightButtonClicked()
+    public void TopRightButtonClicked()
     {
-        Debug.Log("right button is clicked");
+        topCount++;
+
+        if (topCount.Equals(sprites.Length))
+        {
+            topCount = 0;
+        }
+
+        topPlanetImage.sprite = sprites[topCount];
+        
+    }  public void BottomLeftButtonClicked()
+    {
+        bottomCount--;
+
+        if (bottomCount <= 0)
+        {
+            bottomCount = sprites.Length-1;
+        }
+
+        bottomPlanetImage.sprite = sprites[bottomCount];
+    }
+    public void BottomRightButtonClicked()
+    {
+        bottomCount++;
+
+        if (bottomCount.Equals(sprites.Length))
+        {
+            bottomCount = 0;
+        }
+
+        bottomPlanetImage.sprite = sprites[bottomCount];
+        
     }
 }
